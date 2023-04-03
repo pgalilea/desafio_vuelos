d8b05a244c2ac1d4ec2f3a97b6d269acf91acb91
Pablo Galilea <pablog@inexia.cl>
2023-04-02 21:32:52 -0300

    feat: add requirements.txt file

---
+++ run.py
@@ -0,0 +1,6 @@
+# ONLY FOR DEVELOPMENT PURPOSES
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run('app:app', host='0.0.0.0', port=8000, reload=True, log_level="info") # DO NOT use this in production
